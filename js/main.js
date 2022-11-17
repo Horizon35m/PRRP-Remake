a10b63caa9ba6ac9efe3ba5443ec23c44d3505cf
Horizon35m <92321696+Horizon35m@users.noreply.github.com>
2022-11-17 06:25:59 -0500

    updated header with drop menu for mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
+const dropmenu = document.querySelector('.dropmenu')
 const homebtn = document.querySelector('.homebtn');
 const rulesbtn = document.querySelector('.rulesbtn');
 const aboutbtn = document.querySelector('.aboutbtn');
@@ -6,5 +7,6 @@ const applybtn = document.querySelector('.applybtn');
 
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
+    dropmenu.classList.toggle('is-active');
 });
 
